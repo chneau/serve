3e6bd9044d1b97d407503e30ee3d41f3c8738494
chneau <charles63500@gmail.com>
2022-08-10 23:12:12 +0100

    with cool drawbar

--- sendFile.go
+++ sendFile.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mitchellh/ioprogress"
 	"github.com/samber/lo"
 	"github.com/urfave/cli/v2"
 )
@@ -15,6 +18,10 @@ func sendFileAction(c *cli.Context) error {
 	fileName := c.Args().First()
 	fileInfo := lo.Must(os.Stat(fileName))
 	r := gin.Default()
+	bar := ioprogress.DrawTextFormatBar(40)
+	drawFunc := func(progress, total int64) string {
+		return fmt.Sprintf("%s %s\r", bar(progress, total), ioprogress.DrawTextFormatBytes(progress, total))
+	}
 	r.GET("/", func(c *gin.Context) {
 		header := c.Writer.Header()
 		header["Content-Type"] = []string{"application/octet-stream"}
@@ -22,7 +29,12 @@ func sendFileAction(c *cli.Context) error {
 		header["Content-Disposition"] = []string{"attachment; filename=" + fileInfo.Name()}
 		f := lo.Must(os.Open(fileName))
 		defer f.Close()
-		lo.Must(io.Copy(c.Writer, f))
+		lo.Must(io.Copy(c.Writer, &ioprogress.Reader{
+			Reader:       f,
+			Size:         fileInfo.Size(),
+			DrawFunc:     ioprogress.DrawTerminalf(os.Stdout, drawFunc),
+			DrawInterval: time.Millisecond * 100,
+		}))
 		defer os.Exit(0)
 	})
 	printIP(port)
